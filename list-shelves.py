f9891e7dbdf718d68d6a185ddfd3fc19c2264374
jeanmarcboite <jeanmarc.boite@gmail.com>
2024-09-24 09:26:24 +0200

    OK with: python list-shelves.py export --column bookshelf -o /wtf/CalibreBooks/epub

--- list-shelves.py
+++ list-shelves.py
@@ -9,53 +9,64 @@ from pathlib import Path
 
 CALIBRE_DATABASE = "/home/box/Desktop/myCalibreLibrary"
 db_con = sqlite3.connect("/home/box/Desktop/myCalibreLibrary/metadata.db")
+logger = logging.getLogger(__name__)
+
+def set_logger():
+    logger.setLevel(logging.ERROR)
+    # create console handler and set level to debug
+    ch = logging.StreamHandler()
+    ch.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+    logger.addHandler(ch)
+    # 'application' code
+    logger.debug('debug message')
+
+def fetchall(command):
+    cursor = db_con.cursor()
+    rows = cursor.execute(command).fetchall()
+    logger.debug(f'{command}: {rows}')
+
+    return rows
 
 def export_custom_column(column: str, output: str, debug: bool = False):
-  logging.getLogger().setLevel(logging.DEBUG)
-  cur = db_con.cursor()
-  try:
-    cur.execute(f'select id, label from custom_columns')
-    rows = cur.fetchall()
-    logging.debug(rows)
-    for row in filter(lambda r: r[1] == column, rows):
-        logging.debug(row)
-        cur.execute(f'select id, value from custom_column_{row[0]} order by value')
-        shelves = cur.fetchall()
-        for shelf in shelves:
-          display(shelf, output, row[0], debug)
-
-  except Exception as e:
-      logging.error(e)
-
-  db_con.close()
-
-def display(shelf, output, col, debug = False):
-  if debug:
-    print(shelf)
-  print(shelf)
-  Path(f'{output}/{shelf[1]}').mkdir(parents=True, exist_ok=True)
-  cur = db_con.cursor()
-  select = f'select book from books_custom_column_{col}_link where value == {shelf[0]}'
-  cur.execute(select)
-  books = cur.fetchall()
-  book_list = []
-  for book in books:
-    select = f'select title, sort, path from books where id == {book[0]}'
-    b = cur.execute(select).fetchall()[0]
-    book_list.append(b)
-  for book in sorted(book_list, key=lambda bk: bk[1]):
     if debug:
-      print(book[0])
-      print(book[2])
-    ebook_dir = f'{CALIBRE_DATABASE}/{book[2]}'
-    only_files = [f for f in listdir(ebook_dir) if os.path.isfile(os.path.join(ebook_dir, f))]
-    for f in [s for s in only_files if s.endswith(".epub")]:
-        print(f'epub: {CALIBRE_DATABASE}/{f}')
-        # subprocess.call(["rsync", f'{ebook_dir}/{f}', f'{output}/{shelf[1]}'])
+        logger.setLevel(logging.DEBUG)
+    try:
+        rows = fetchall(f'select id, label from custom_columns where label == \"{column}\"')
+        for row in rows:
+            values = fetchall(f'select value, id from custom_column_{row[0]} order by value')
+            for value in values:
+                export(value[0], value[1], output, row[0], debug)
+
+    except sqlite3.OperationalError as e:
+        logger.error(f'{type(e)}: {e}')
+
+    db_con.close()
+
+
+def export(shelf, shelf_id, output, col, debug=False):
+    output_directory = f'{output}/{shelf}'
+    logger.warning(f'Create directory {output_directory}')
+    Path(output_directory).mkdir(parents=True, exist_ok=True)
+    books = fetchall(f'select book from books_custom_column_{col}_link where value == {shelf_id}')
+    book_list = []
+    for book in books:
+        b = fetchall(f'select title, sort, path from books where id == {book[0]}')
+        book_list.append(b[0])
+    logger.debug(f'books: {book_list}')
+    for book in sorted(book_list, key=lambda bk: bk[1]):
+        ebook_dir = f'{CALIBRE_DATABASE}/{book[2]}'
+        only_files = [f for f in listdir(ebook_dir) if os.path.isfile(os.path.join(ebook_dir, f))]
+        for f in [s for s in only_files if s.endswith(".epub")]:
+            logger.debug(f'rsync {ebook_dir}/{f} {output}/{shelf}')
+            subprocess.call(["rsync", f'{ebook_dir}/{f}', f'{output}/{shelf}'])
+
+
 def main():
-  fire.Fire({
-    'export' : export_custom_column,
-  })
+    fire.Fire({
+        'export': export_custom_column,
+    })
+
 
 if __name__ == '__main__':
-  main()
+    set_logger()
+    main()
