85029b85427ab39e9171d9e2edab1dce0da3f4e8
jmboite <jeanmarc.boite@gmail.com>
2024-11-19 08:02:01 +0100

    export

--- export.sh
+++ export.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 library=/home/box/Desktop/myCalibreLibrary
 output=/wtf/CalibreBooks
-python calibre-export.py column --library $library --label bookshelf -o $output/epub
-python calibre-export.py column --library $library --label bookshelf -o $output/pdf -f pdf
-python calibre-export.py column --library $library --label read -o $output/read
-python calibre-export.py column --library $library --label reading -o $output/reading
-python calibre-export.py column --library $library --label read -o $output/read -f pdf
-python calibre-export.py column --library $library --label reading -o $output/reading -f pdf
-python calibre-export.py column --library $library --label read --value 0 -o $output/unread
-python calibre-export.py authors --library $library  -o $output/authors
-python calibre-export.py series --library $library  -o $output/series
-python calibre-export.py tags --library $library  -o $output/tags
+exe=/home/box/src/python/calibre/cdb/calibre-export.py
+python $exe column --library $library --label bookshelf -o $output/epub
+python $exe column --library $library --label bookshelf -o $output/pdf -f pdf
+python $exe column --library $library --label read -o $output/read
+python $exe column --library $library --label reading -o $output/reading
+python $exe column --library $library --label read -o $output/read -f pdf
+python $exe column --library $library --label reading -o $output/reading -f pdf
+python $exe column --library $library --label read --value 0 -o $output/unread
+python $exe authors --library $library  -o $output/authors
+python $exe series --library $library  -o $output/series
+python $exe tags --library $library  -o $output/tags
